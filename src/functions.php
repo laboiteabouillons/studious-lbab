15775422203a873a8e823f9d0552f6dd924cb07f
ojullien <ojullien@users.noreply.github.com>
2018-10-15 17:09:34 +0200

    Work in Progress
    Signed-off-by: ojullien <ojullien@users.noreply.github.com>

--- src/functions.php
+++ src/functions.php
@@ -13,5 +13,5 @@ echo basename(__FILE__), PHP_EOL;
 /**
  * Remove Filters
  */
-remove_filter( 'the_content', 'wpautop' ); // Stop WP adding extra <p> </p> to the pages' content
-remove_filter( 'the_excerpt', 'wpautop' ); // Stop WP adding extra <p> </p> to the post excerpts' content
+//remove_filter( 'the_content', 'wpautop' ); // Stop WP adding extra <p> </p> to the pages' content
+//remove_filter( 'the_excerpt', 'wpautop' ); // Stop WP adding extra <p> </p> to the post excerpts' content
