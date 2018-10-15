6e7317d3d40b51ca694589e798f48a269e381f61
ojullien <ojullien@users.noreply.github.com>
2018-10-15 20:19:58 +0200

    Work in Progress
    Signed-off-by: ojullien <ojullien@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # studious-lbab
 Development repository for the *La boîte à bouillons* WordPress theme.
+
+I do not plan on releasing the theme to a larger audience and I do not ensure a backward compatibility.
--- src/header.php
+++ src/header.php
@@ -1,7 +1,8 @@
 <?php
 declare(strict_types=1);
 /**
- * The header template file usually contains your site’s document type, meta
+ *
+ * The header template partial usually contains your site’s document type, meta
  * information, links to stylesheets and scripts, and other data.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
