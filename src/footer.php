eb2d368c6ab78e7e0c82f499c4ad4cdd0c5685d8
ojullien <ojullien@users.noreply.github.com>
2018-10-16 21:47:43 +0200

    Work in progress
    Signed-off-by: ojullien <ojullien@users.noreply.github.com>

--- src/footer.php
+++ src/footer.php
@@ -1,13 +1,22 @@
 <?php
-declare(strict_types=1);
 /**
- * The template for displaying the footer.
+ * The footer template partial usually contains your site’s credits, widgetized areas, etc.
  *
+ * The template tag to use to include this template as a part of another template is: get_footer()
+ * Any footer content should be hooked into the wp_footer filter.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  * @link https://developer.wordpress.org/themes/template-files-section/partial-and-miscellaneous-template-files/#footer-php
  *
- * @package La boîte à bouillons
+ * @package studious-lbab
  */
 
-echo basename(__FILE__), PHP_EOL;
+declare(strict_types=1);
+?>
+<footer>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
+<?php
+echo esc_html( basename( __FILE__ ) ), PHP_EOL;
--- src/header.php
+++ src/header.php
@@ -1,9 +1,11 @@
 <?php
-declare(strict_types=1);
 /**
+ * The header template partial usually contains your site’s document type, meta information, links to stylesheets and
+ * scripts, and other data.
  *
- * The header template partial usually contains your site’s document type, meta
- * information, links to stylesheets and scripts, and other data.
+ * The template tag to use to include this template as a part of another template is: get_header()
+ * Any head content should be hooked into the wp_head filter.
+ * Styles and scripts are enqueued through the wp_enqueue_script function.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  * @link https://developer.wordpress.org/themes/template-files-section/partial-and-miscellaneous-template-files/#header-php
@@ -11,4 +13,14 @@ declare(strict_types=1);
  * @package studious-lbab
  */
 
-echo basename(__FILE__), PHP_EOL;
+declare(strict_types=1);
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<?php wp_head(); ?>
+<link rel="profile" href="https://gmpg.org/xfn/11">
+</head>
+<body <?php body_class(); ?>>
+<?php
+echo esc_html( basename( __FILE__ ) ), PHP_EOL;
